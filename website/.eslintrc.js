54986bef5f8e9ff285101dc4bd45f943d15fa155
Aurélie VIOLETTE <orwel.violette@gmail.com>
2021-02-15 23:32:33 +0100

    :rotating_light: Update eslintconfig

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,12 +1,26 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: false,
+      tsx: false,
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-recommanded',
+    'plugin:vue/vue3-recommended',
     'prettier',
     'prettier/vue',
     'prettier/@typescript-eslint',
+  ],
+  overrides: [
+    {
+      files: ['.eslintrc.js', 'tailwind.config.js'],
+      env: {
+        node: true,
+      },
+    },
   ],
 }
